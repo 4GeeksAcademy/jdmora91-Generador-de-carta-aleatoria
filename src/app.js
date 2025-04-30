c6b16a294e285a5a94540712fb99c31575dec5ba
Jose Mora <j.donismora@gmail.com>
2025-04-30 22:40:27 +0000

    elaboracionDeCartaAleatoria

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  const palos = ["♥", "♦", "♠", "♣"];
+  const clasesPalos = ["heart", "diamond", "spade", "club"];
+  const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
+  const carta = document.getElementById("carta");
+  const valorCarta = document.getElementById("valor-carta");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const paloArribaIzq = document.getElementById("palo-arriba-izq");
+  const paloAbajoDer = document.getElementById("palo-abajo-der");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const indicePalo = Math.floor(Math.random() * palos.length);
+  const simboloPalo = palos[indicePalo];
+  const clasePalo = clasesPalos[indicePalo];
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+
+ 
+  carta.classList.remove("heart", "diamond", "spade", "club");
+
+  
+  carta.classList.add(clasePalo);
+
+  
+  valorCarta.innerText = valor;
+  paloArribaIzq.innerText = simboloPalo;
+  paloAbajoDer.innerText = simboloPalo;
 };
